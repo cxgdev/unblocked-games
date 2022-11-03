a2bba872f210ab2faf1b23b2023246418ebc8ad2
Chase Castora <81863549+cxgdev@users.noreply.github.com>
2022-11-03 08:52:34 -0400

    Update game-scripts.js

--- game-scripts.js
+++ game-scripts.js
@@ -32,6 +32,11 @@ fetch('./game.json')
         developer = data.developer;
     });
 
+document.open();
+document.write(`<div class="container"><div class="game-container" id="gm4html5_div_id"><iframe frameborder="0" allowfullscreen="" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true" type="text/html" src="./game/index.html" id="game-iframe" style="aspect-ratio: 16 / 9;" width="100%"></iframe><div class="game-container__info"><div class="game-container__info__text"><span class="game-container__info-heading" id="game-title">${title}</span><span class="game-container__info-description" id="game-dev">by ${developer}</span></div><button class="mdc-button mdc-button--raised" id="fullscreen"><span class="mdc-button__ripple"></span><span class="mdc-button__label">FULLSCREEN</span></button></div></div></div>`);
+document.close();
+
+/*
 // Main Container
 var container = document.createElement('div');
 container.classList.add('container');
@@ -81,7 +86,7 @@ gameContainer.appendChild(infoContainer);
 container.appendChild(gameContainer);
 
 document.body.appendChild(container);
-
+*/
 
 fullscreenButton.addEventListener("click", () => {
     if (gameEmbed.requestFullscreen) {
